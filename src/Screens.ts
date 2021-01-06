cb39acef4a38a988873c17976b6a86f43f8afb74
JustDave2002 <elsh0009@hz.nl>
2021-01-06 16:09:19 +0100

    tryina fix these fuckin things

--- src/Screens.ts
+++ src/Screens.ts
@@ -8,4 +8,33 @@ abstract class Screens{
     protected draw() {
         
     }
+
+    /**
+     * Create a random scoring object and clear the other scoring objects by setting them to `null`.
+     */
+    
+    /**
+   * Writes text to the canvas
+   * @param {string} text - Text to write
+   * @param {number} fontSize - Font size in pixels
+   * @param {number} xCoordinate - Horizontal coordinate in pixels
+   * @param {number} yCoordinate - Vertical coordinate in pixels
+   * @param {string} alignment - Where to align the text
+   * @param {string} color - The color of the text
+   */
+  public writeTextToCanvas(
+    ctx: CanvasRenderingContext2D,
+    text: string,
+    xCoordinate: number,
+    yCoordinate: number,
+    fontSize: number = 20,
+    color: string = "red",
+    alignment: CanvasTextAlign = "center"
+) {
+    ctx.font = `${fontSize}px sans-serif`;
+    ctx.fillStyle = color;
+    ctx.textAlign = alignment;
+    ctx.fillText(text, xCoordinate, yCoordinate);
+}
+
 }
