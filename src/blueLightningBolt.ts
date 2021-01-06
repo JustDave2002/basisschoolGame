e56d2f3a32f0958a12f7fb85b94c5404167ce07e
BuildTools <unconfigured@null.spigotmc.org>
2021-01-06 16:24:13 +0100

    push

--- src/blueLightningBolt.ts
+++ src/blueLightningBolt.ts
@@ -7,8 +7,8 @@ class BlueLightningBolt extends ScoringObject {
         super(canvas);
         this.image = this.loadNewImage("assets/img/objects/face_on_blue_power_icon.png");
         this.speed = 7;
-        this.points = -15;
-        this._lives = 0;
+        this.points = 0;
+        this._lives = -1;
     }
 }
 
