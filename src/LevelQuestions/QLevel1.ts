6aa46ef67cc0ab634eb48f0f47bbd8fdb4e4b98e
JustDave2002 <elsh0009@hz.nl>
2021-01-17 16:59:16 +0100

    bugfix

--- src/LevelQuestions/QLevel1.ts
+++ src/LevelQuestions/QLevel1.ts
@@ -7,14 +7,14 @@ class QLevel1 extends Questions {
         super(canvas, player)
         this.questionArray = [
             {
-                question: [" Wat moet je doen wanneer je een email krijgt van een onbekenden?"],
+                question: [" Wat moet je doen wanneer je een email krijgt van een onbekenden?",""],
                 choices: ["verwijder de email ", " rapporteer en blokkeer "],
                 answer: 2, 
                 explanation:[ "  Je kan zo zorgen dat google  er iets tegen kan doen.", 
                 "Ook krijg je dan geen emails meer."]
             
             },{
-                question: ["Wat is een goed wachtwoord?"],
+                question: ["Wat is een goed wachtwoord?",""],
                  choices: ["1234", " W0nderwa11$"],
                  answer: 2, 
                  explanation: ["1234 is makkelijk om achter te komen voor hackers.",
@@ -22,27 +22,27 @@ class QLevel1 extends Questions {
 
             
             },{
-                question: ["Tegen welke mensen vertel je inloggegevens?"],
+                question: ["Tegen welke mensen vertel je inloggegevens?",""],
                 choices: ["Je ouders", "Je vrienden"],
                 answer: 1, 
                 explanation: [" Je ouders houden het geheim ",
                               "Bij je vrienden weet je dat niet zeker."]
             
             },{
-                question: [" Iemand stuurt je een sms en hij stelt persoonlijke vragen."],
+                question: [" Iemand stuurt je een sms en hij stelt persoonlijke vragen.",""],
                 choices: ["Blokkeer diegene","Ga met de persoon in gesprek"],
                 answer: 1, 
                 explanation: ["Reageer nooit op deze berichten en deel zeker nooit informatie.",
                             "Hierdoor weten ze niks"]  
             
             },{
-                question: [" Welke dingen deel je niet op social media? "],
+                question: [" Welke dingen deel je niet op social media? ",""],
                 choices: ["je adres "," je naam"],
                 answer: 1, 
                 explanation:[ "Deze info is voor vrienden niet iedereen."]
             
             },{
-                question: ["Is het oké om je je ouders betalings informatie op het internet te delen?"],
+                question: ["Is het oké om je je ouders betalings informatie op het internet te delen?",""],
                 choices: ["Ja", "Nee"],
                 answer: 2, 
                 explanation:[ "Deel nooit prive informatie."]
